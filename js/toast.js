fc770412b91efa0b5805a136bf8a36ef30909127
admin <admin@admins-MacBook-Pro-3.local>
2024-01-05 21:17:06 +0900

    feat:공용 컴포넌트

---
+++ js/toast.js
@@ -0,0 +1,15 @@
+const Toast = (message) => {
+  Toastify({
+    text: `${message}`,
+    duration: 2500,
+    destination: "https://github.com/apvarun/toastify-js",
+    newWindow: true,
+    close: false,
+    gravity: "bottom", // `top` or `bottom`
+    position: "center", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {},
+    backgroundColor: "rgba(82, 180, 204, 1)",
+    onClick: function () {}, // Callback after click
+  }).showToast();
+};
