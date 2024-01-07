81021b34533734cc459986318d0c6c0d0d6eb79a
sean2337 <sean2337@naver.com>
2024-01-07 14:28:31 +0900

    design: 상세페이지 QA 1차 진행 수정

--- js/components/back.js
+++ js/components/back.js
@@ -1,3 +1,13 @@
-document.getElementById("bigTitle").addEventListener("click", function () {
+function handleBackClick() {
   window.history.back();
-});
+}
+
+var bigTitleElement = document.getElementById("bigTitle");
+if (bigTitleElement) {
+  bigTitleElement.addEventListener("click", handleBackClick);
+}
+
+var backWhiteElement = document.getElementById("backWhite");
+if (backWhiteElement) {
+  backWhiteElement.addEventListener("click", handleBackClick);
+}
