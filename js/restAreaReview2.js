282b8a6a5897d350a09da52fa4f0d789f6119379
sean2337 <sean2337@naver.com>
2024-01-08 23:03:51 +0900

    design: 240108 QA 완료

--- js/restAreaReview2.js
+++ js/restAreaReview2.js
@@ -1,33 +1,34 @@
-
-const dataReceived = JSON.parse(localStorage.getItem('myData'));
+const dataReceived = JSON.parse(localStorage.getItem("myData"));
 
 const submit = document.querySelector("#writeContinueBtn");
 
 if (dataReceived) {
-    // Use the data
-    console.log(dataReceived);
+  // Use the data
+  console.log(dataReceived);
 } else {
-    console.log("not received");
-    // Handle the case where data is not available
+  console.log("not received");
+  // Handle the case where data is not available
 }
 
-
-var RestaurantList = ["풀초롱 밥상", "풀초롱 밥상머리 밥상", "쳐비스","asdasd","DSfsdf"];
+var RestaurantList = [
+  "풀초롱 밥상",
+  "풀초롱 밥상머리 밥상",
+  "쳐비스",
+  "asdasd",
+  "DSfsdf",
+];
 /// RestaurantList 는 가변 변수다.
 
 const SELECTED = [];
 
 var selectedJson = {
-    "selected" : SELECTED
+  selected: SELECTED,
 };
-
-
 
 const container = document.querySelector("#RestaurantList");
 const Modal = document.querySelector("#Modal");
-const inputBar = document.querySelector('.inputBar');
+const inputBar = document.querySelector(".inputBar");
 const Star = document.getElementsByTagName("input");
-
 
 var curPos = 0;
 var prevPos = 0;
@@ -39,77 +40,66 @@ var lightblue = "#f1fafc";
 
 Modal.style.height = H;
 RestaurantList.forEach((item, index) => {
-    const singleBtn = document.createElement('div');
-    singleBtn.classList.add('RestaurantBtn');
-    const width = item.length * 13.5;
-    singleBtn.style.width = `${width}px`;
-    singleBtn.innerText = item;
+  const singleBtn = document.createElement("div");
+  singleBtn.classList.add("RestaurantBtn");
+  const width = item.length * 13.5;
+  singleBtn.style.width = `${width}px`;
+  singleBtn.innerText = item;
 
-    
-    singleBtn.style.marginBottom = "10px";
+  singleBtn.style.marginBottom = "10px";
 
-    singleBtn.addEventListener("click", (event)=>{
-        var backgroundColor = window.getComputedStyle(event.target).backgroundColor;
-        if(SELECTED.includes(event.target.innerText)){
-            for(let i = 0; i < SELECTED.length; i++){
-                if(SELECTED[i] === event.target.innerText){
-                    SELECTED.splice(i,1);
-                }
-            }
+  singleBtn.addEventListener("click", (event) => {
+    var backgroundColor = window.getComputedStyle(event.target).backgroundColor;
+    if (SELECTED.includes(event.target.innerText)) {
+      for (let i = 0; i < SELECTED.length; i++) {
+        if (SELECTED[i] === event.target.innerText) {
+          SELECTED.splice(i, 1);
         }
-        else{
-            SELECTED.push(event.target.innerText);
-        }
-        if(backgroundColor === gray){
+      }
+    } else {
+      SELECTED.push(event.target.innerText);
+    }
+    if (backgroundColor === gray) {
+      event.target.style.background = lightblue;
+      event.target.style.color = skyblue;
+      event.target.style.border = `0.79px solid ${skyblue}`;
+    } else {
+      event.target.style.background = gray;
+      event.target.style.color = "#999ba5";
+      event.target.style.border = `0.79px solid #999ba5`;
+    }
+    selectedJson = {
+      selected: SELECTED,
+    };
 
-            event.target.style.background = lightblue;
-            event.target.style.color = skyblue;
-            event.target.style.border = `0.79px solid ${skyblue}`;
-        }
-        else{
+    console.log(selectedJson["selected"]);
+  });
 
-            event.target.style.background = gray;
-            event.target.style.color = "#999ba5";
-            event.target.style.border = `0.79px solid #999ba5`;
-        }
-        selectedJson = {
-            "selected": SELECTED
-        };
+  Modal.appendChild(singleBtn);
+  const pos = singleBtn.getBoundingClientRect();
 
-        console.log(selectedJson["selected"]);
-
-
-    });
-
-
-    Modal.appendChild(singleBtn);
-    const pos = singleBtn.getBoundingClientRect();
-    
-    
-    H = pos.top;
-    Modal.style.height = `${H}px`;
-    
+  H = pos.top;
+  Modal.style.height = `${H}px`;
 });
 
-
-const inputField = document.createElement('div');
-inputField.classList.add('inputBar');
+const inputField = document.createElement("div");
+inputField.innerHTML = `<div class="dropdown">
+<button id="dropDownBtn">메뉴를 선택해주세요</button>
+<div style="display: none" id="drop-content"></div>
+</div>`;
+inputField.classList.add("inputBar");
 Modal.appendChild(inputField);
 
+submit.addEventListener("click", () => {
+  var appending = [];
+  console.log(Star[0].value);
+  for (let i = 0; i < Star.length; i++) {
+    appending.push({
+      [`${selectedJson["selected"][i]}`]: Star[i].value,
+    });
+  }
+  dataReceived["selected"] = appending;
+  console.log(dataReceived);
 
-
-submit.addEventListener("click",()=>{
-    
-    var appending = [];
-    console.log(Star[0].value);
-    for (let i = 0; i < Star.length; i++) {
-        appending.push({
-            [`${selectedJson["selected"][i]}`]: Star[i].value
-        });
-    }
-    dataReceived["selected"] = appending;
-    console.log(dataReceived);
-
-
-    var finalJson = dataReceived;
+  var finalJson = dataReceived;
 });
