38de0e21ac9e4c24ebb61bbfdbe9ff4e1dd8e25f
sean2337 <sean2337@naver.com>
2024-01-08 19:17:10 +0900

    design: 테마 별 아이콘 로직 생성 및 적용

--- js/pages/main/choiceTheme.js
+++ js/pages/main/choiceTheme.js
@@ -1,16 +1,51 @@
 var unChoiceThemes = document.querySelectorAll("#unChoiceTheme");
 var choice = document.getElementById("choiceTheme");
+var plusFileUrl = "/assets/img/themeImg/";
 
 choice.addEventListener("click", function () {
+  if (choice.textContent !== "모든 휴게소") {
+    var beforeImgTag = choice.getElementsByTagName("img");
+    var newImgUrl = beforeImgTag[0].src;
+    var newUrl =
+      plusFileUrl +
+      "gray" +
+      newImgUrl.substring(newImgUrl.lastIndexOf("/") + 1);
+    beforeImgTag[0].src = newUrl;
+  }
   choice.id = "unChoiceTheme";
   choice = this;
   choice.id = "choiceTheme";
-});
 
+  if (choice.textContent !== "모든 휴게소") {
+    var beforeImgTag = choice.getElementsByTagName("img");
+    var newImgUrl = beforeImgTag[0].src;
+    var newUrl =
+      plusFileUrl + newImgUrl.substring(newImgUrl.lastIndexOf("/") + 5);
+    beforeImgTag[0].src = newUrl;
+  }
+});
+//"/assets/img/themeImg/graybathroom.png"
 unChoiceThemes.forEach((unChoiceTheme) => {
   unChoiceTheme.addEventListener("click", function () {
+    if (choice.textContent !== "모든 휴게소") {
+      var beforeImgTag = choice.getElementsByTagName("img");
+      var newImgUrl = beforeImgTag[0].src;
+      var newUrl =
+        plusFileUrl +
+        "gray" +
+        newImgUrl.substring(newImgUrl.lastIndexOf("/") + 1);
+      beforeImgTag[0].src = newUrl;
+    }
     choice.id = "unChoiceTheme";
     choice = this;
     choice.id = "choiceTheme";
+
+    if (choice.textContent !== "모든 휴게소") {
+      var beforeImgTag = choice.getElementsByTagName("img");
+      var newImgUrl = beforeImgTag[0].src;
+      var newUrl =
+        plusFileUrl + newImgUrl.substring(newImgUrl.lastIndexOf("/") + 5);
+      beforeImgTag[0].src = newUrl;
+    }
   });
 });
