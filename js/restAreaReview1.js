d9625c8d0246410b79da138b25e61cea27b1a7c4
zensen6 <junhee1469@gmail.com>
2024-01-10 04:21:31 +0900

    final??

--- js/restAreaReview1.js
+++ js/restAreaReview1.js
@@ -1,5 +1,4 @@
-//import Toast from "/js/toast.js";
-// 새로운 script 요소를 생성합니다.
+import axios from 'axios';
 const scriptElement = document.createElement("script");
 // src 속성을 설정합니다.
 scriptElement.src = "https://cdn.jsdelivr.net/npm/toastify-js";
@@ -23,6 +22,8 @@ const Toast = (message) => {
   }).showToast();
 };
 
+(async () => {
+
 const finishBtn = document.querySelector("#fillBtn");
 const notFinishBtn = document.querySelector("#lineBtn");
 
@@ -30,11 +31,34 @@ const starList = document.getElementsByTagName("input");
 
 var finishJson = {};
 
-finishBtn.addEventListener("click", () => {
-  finishJson["음식이 다양해요"] = starList[0].value;
-  finishJson["시설이 편리해요"] = starList[1].value;
-  finishJson["화장실이 깨끗해요"] = starList[2].value;
-  finishJson["분위기가 특별해요"] = starList[3].value;
+var restAreaId = localStorage.getItem("restPlaceId");
+console.log("restAREA:: ", restAreaId);
+
+finishBtn.addEventListener("click", async() => {
+    console.log("restArea: ");
+    /*
+  finishJson["food"] = parseFloat(starList[0].value) +.0;
+  finishJson["amenities"] = parseFloat(starList[1].value) + .0;
+  finishJson["restRoom"] = parseFloat(starList[2].value) +.0;
+  finishJson["vibe"] = parseFloat(starList[3].value) +.0;
+  finishJson["restAreaId"] = parseInt(restAreaId);
+  */
+
+  const finishJson = {
+    food: parseFloat(starList[0].value) + 0.0,
+    amenities: parseFloat(starList[1].value) + 0.0,
+    restRoom: parseFloat(starList[2].value) + 0.0,
+    vibe: parseFloat(starList[3].value) + 0.0,
+    restAreaId: parseInt(restAreaId),
+  };
+
+  const params = new URLSearchParams();
+    params.append('food', finishJson.food);
+    params.append('amenities', finishJson.amenities);
+    params.append('restRoom', finishJson.restRoom);
+    params.append('vibe', finishJson.vibe);
+    params.append('restAreaId', finishJson.restAreaId);
+
   var c = 0;
   if (starList[0].value === "0") c++;
   if (starList[1].value === "0") c++;
@@ -43,16 +67,41 @@ finishBtn.addEventListener("click", () => {
   if (c >= 3) {
     Toast("2개 이상 점수를 입력해주세요.");
   } else {
-    localStorage.setItem("myData", JSON.stringify(finishJson));
-    window.location.href = "/reviewComplete";
+
+    const json = JSON.stringify(finishJson);
+
+    const response = await axios
+    .post(`http://15.164.44.233:8080/rest-area/${restAreaId}/review`, 
+        params
+    )
+    .then((data) => {
+        console.log("succcceesss");
+        console.log(data);
+
+        //localStorage.setItem("myData", JSON.stringify(finishJson));
+        window.location.href = "/reviewComplete";
+
+    })
+    .catch((error)=>{
+        console.log(error);
+    });
+
   }
 });
 
-notFinishBtn.addEventListener("click", () => {
-  finishJson["음식이 다양해요"] = starList[0].value / 2;
-  finishJson["시설이 편리해요"] = starList[1].value / 2;
-  finishJson["화장실이 깨끗해요"] = starList[2].value / 2;
-  finishJson["분위기가 특별해요"] = starList[3].value / 2;
+
+
+
+
+
+
+notFinishBtn.addEventListener("click", async() => {
+
+
+  //finishJson["음식이 다양해요"] = starList[0].value / 2;
+  //finishJson["시설이 편리해요"] = starList[1].value / 2;
+  //finishJson["화장실이 깨끗해요"] = starList[2].value / 2;
+  //finishJson["분위기가 특별해요"] = starList[3].value / 2;
 
   var c = 0;
   if (starList[0].value === "0") c++;
@@ -62,7 +111,51 @@ notFinishBtn.addEventListener("click", () => {
   if (c >= 3) {
     Toast("2개 이상 점수를 입력해주세요.");
   } else {
+
+    const finishJson = {
+        food: parseFloat(starList[0].value) + 0.0,
+        amenities: parseFloat(starList[1].value) + 0.0,
+        restRoom: parseFloat(starList[2].value) + 0.0,
+        vibe: parseFloat(starList[3].value) + 0.0,
+        restAreaId: parseInt(restAreaId),
+      };
+
+
+    const params = new URLSearchParams();
+    params.append('food', finishJson.food);
+    params.append('amenities', finishJson.amenities);
+    params.append('restRoom', finishJson.restRoom);
+    params.append('vibe', finishJson.vibe);
+    params.append('restAreaId', finishJson.restAreaId);
+
+    
+    const response = await axios
+    .get(`http://15.164.44.233:8080/rest-area/${restAreaId}/restaurants/names`)
+    .then((data) => {
+      console.log("successsss2");
+      console.log(data);
+    })
+    .catch(error => {
+        console.log(error);
+
+    });
+    
+  
     localStorage.setItem("myData", JSON.stringify(finishJson));
     window.location.href = "/restAreaReview2";
   }
+
+  
 });
+
+
+
+})();
+/*
+const response = await axios
+    .get(`http://15.164.44.233:8080/rest-areas/${choicePlaceId}/details`, {})
+    .then((data) => {
+
+    });
+
+*/
