a8d5239de8db249b320313ddec59e80e5db13609
sean2337 <sean2337@naver.com>
2024-01-10 04:26:41 +0900

    chore: pull전 커밋

--- js/restAreaReview1.js
+++ js/restAreaReview1.js
@@ -1,5 +1,4 @@
-//import Toast from "/js/toast.js";
-// 새로운 script 요소를 생성합니다.
+import axios from "axios";
 const scriptElement = document.createElement("script");
 // src 속성을 설정합니다.
 scriptElement.src = "https://cdn.jsdelivr.net/npm/toastify-js";
@@ -30,11 +29,17 @@ const starList = document.getElementsByTagName("input");
 
 var finishJson = {};
 
-finishBtn.addEventListener("click", () => {
-  finishJson["음식이 다양해요"] = starList[0].value;
-  finishJson["시설이 편리해요"] = starList[1].value;
-  finishJson["화장실이 깨끗해요"] = starList[2].value;
-  finishJson["분위기가 특별해요"] = starList[3].value;
+var restAreaId = localStorage.getItem("restPlaceId");
+console.log("restAREA:: ", restAreaId);
+
+finishBtn.addEventListener("click", async () => {
+  console.log("restArea: ");
+  finishJson["food"] = parseFloat(starList[0].value) + 0.0;
+  finishJson["amenities"] = parseFloat(starList[1].value) + 0.0;
+  finishJson["restRoom"] = parseFloat(starList[2].value) + 0.0;
+  finishJson["vibe"] = parseFloat(starList[3].value) + 0.0;
+  finishJson["restAreaId"] = parseInt(restAreaId);
+  console.log(finishJson);
   var c = 0;
   if (starList[0].value === "0") c++;
   if (starList[1].value === "0") c++;
@@ -43,12 +48,33 @@ finishBtn.addEventListener("click", () => {
   if (c >= 3) {
     Toast("2개 이상 점수를 입력해주세요.");
   } else {
-    localStorage.setItem("myData", JSON.stringify(finishJson));
-    window.location.href = "/reviewComplete";
+    const response = await axios
+      .post(
+        `http://15.164.44.233:8080/rest-area/${restAreaId}/review`,
+        JSON.stringify({
+          food: starList[0].value / 2,
+          amenities: starList[1].value / 2,
+          restRoom: starList[2].value / 2,
+          vibe: starList[3].value / 2,
+        }),
+        { headers: { "Content-Type": "application/json" } }
+      )
+      .then((data) => {
+        console.log("succcceesss");
+        console.log(data);
+
+        //localStorage.setItem("myData", JSON.stringify(finishJson));
+        window.location.href = "/reviewComplete";
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 });
 
-notFinishBtn.addEventListener("click", () => {
+var restAreaId = localStorage.getItem("restPlaceId");
+
+notFinishBtn.addEventListener("click", async () => {
   finishJson["음식이 다양해요"] = starList[0].value / 2;
   finishJson["시설이 편리해요"] = starList[1].value / 2;
   finishJson["화장실이 깨끗해요"] = starList[2].value / 2;
@@ -62,7 +88,24 @@ notFinishBtn.addEventListener("click", () => {
   if (c >= 3) {
     Toast("2개 이상 점수를 입력해주세요.");
   } else {
-    localStorage.setItem("myData", JSON.stringify(finishJson));
+    localStorage.setItem(
+      "myData",
+      JSON.stringify({
+        food: starList[0].value / 2,
+        amenities: starList[1].value / 2,
+        restRoom: starList[2].value / 2,
+        vibe: starList[3].value / 2,
+      })
+    );
     window.location.href = "/restAreaReview2";
   }
 });
+
+/*
+const response = await axios
+    .get(`http://15.164.44.233:8080/rest-areas/${choicePlaceId}/details`, {})
+    .then((data) => {
+
+    });
+
+*/
