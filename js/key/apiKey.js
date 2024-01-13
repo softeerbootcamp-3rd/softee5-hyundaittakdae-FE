078d6ad793c6d10b79fcbb874cfd5c7b48c5bdbf
sean2337 <sean2337@naver.com>
2024-01-13 16:11:32 +0900

    refactor: 파일 전체 불필요 코드 제거 및 경량화

--- js/key/apiKey.js
+++
@@ -1,2 +0,0 @@
-const API_KEY = "http://15.164.44.233:8080";
-export default API_KEY;
--- js/key/googleAPIKey.js
+++
@@ -1,2 +0,0 @@
-const GOOGLE_API_KEY = "AIzaSyCw1ZBbZLRARjidn3WHiKdlJThB3a-uGdE";
-export default GOOGLE_API_KEY;
